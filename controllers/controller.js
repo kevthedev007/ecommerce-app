2fc4674d9ca7c13a1f0bc090b4062f6407369130
kevthedev007 <63928159+kevthedev007@users.noreply.github.com>
2021-02-12 20:32:19 +0100

    ok

--- controllers/controller.js
+++ controllers/controller.js
@@ -16,8 +16,7 @@ let controller = {
 
     getCart: async function(req, res) {
         let getItems = await pool.query('SELECT products.name, products.price, cart.cart_id FROM products JOIN cart ON products.id = cart.product_id');
-        let total = await pool.query('SELECT SUM(products.price) FROM products JOIN cart ON products.id = cart.product_id')
-        res.json(total.rows)
+        res.json(getItems.rows)
     },
 
     deleteFromCart: async function(req, res) {
